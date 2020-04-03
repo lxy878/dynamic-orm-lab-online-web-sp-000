f8ae8562af2448b8126d71478d3bbe2150a065a4
Xiaoyi Liu <lxy878@gmail.com>
2020-04-03 14:52:43 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -5,12 +5,6 @@ require 'pry'
 
 class InteractiveRecord
 
-  def initialize(attributes={})
-    attributes.each do |key, value|
-      self.send("#{key}=", value)
-    end
-  end
-
   def self.table_name
     self.to_s.downcase.pluralize
   end
@@ -24,6 +18,12 @@ class InteractiveRecord
     table_info.collect {|hash| hash["name"]}.compact
   end
 
+  def initialize(attributes={})
+    attributes.each do |key, value|
+      self.send("#{key}=", value)
+    end
+  end
+  
   def table_name_for_insert
     self.class.table_name
   end
