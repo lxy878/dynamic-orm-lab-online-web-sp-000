9ad8474c5a1657eabbfc7e95937087bcb6d8da2c
Xiaoyi Liu <lxy878@gmail.com>
2020-04-03 13:49:42 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,15 +2,41 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
+
   def initialize(attributes)
 
   end
 
   def self.table_name
-
+    
   end
 
   def self.column_names
 
   end
+
+  def table_name_for_insert
+
+  end
+
+  def col_names_for_insert
+
+  end
+
+  def values_for_insert
+
+  end
+
+  def save
+
+  end
+
+  def self.find_by_name
+
+  end
+
+  def self.find_by_name
+
+  end
+
 end
