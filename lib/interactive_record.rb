9a6f9cbb5069ae6fa2f729b8019183abfdb5fa2e
Xiaoyi Liu <lxy878@gmail.com>
2020-04-03 15:21:00 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -37,7 +37,7 @@ class InteractiveRecord
     self.class.column_names.each do |column|
       values = "#{send(column)}" unless send(column).nil?
     end
-    # binding.pry
+    binding.pry
   end
 
   def save
